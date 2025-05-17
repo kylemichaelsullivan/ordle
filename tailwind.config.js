118606452f5ef343993ed5d89dd4dad1cbcad9af
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2025-05-17 14:02:12 -0400

    ADD: text-red-400, text-green-400 to TW safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /**  @type {import('tailwindcss').Config}  */
 export default {
-	content: ['./index.html', './src/**/*.{ts,tsx}'],
-	theme: { extend: {} },
-	plugins: [],
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  theme: { extend: {} },
+  plugins: [],
+  safelist: ['text-red-400', 'text-green-400'],
 };
