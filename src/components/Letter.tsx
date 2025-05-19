eed219e6f15163a79efd13cfae83c4cfef4d3c7c
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2025-05-19 18:31:10 -0400

    FIX: Status Color for <Letter />

--- src/components/Letter.tsx
+++ src/components/Letter.tsx
@@ -6,26 +6,26 @@ import LetterButtonUp from '@/components/LetterButtonUp';
 import type { Letter } from '@/types';
 
 type LetterProps = {
-	letter: Letter;
+  letter: Letter;
 };
 
 function Letter({ letter }: LetterProps) {
-	const { lettersStatus } = useOrdle();
-	const status = lettersStatus[letter];
-	const statusColor =
-		status === 0 || status === 1 ? (status === 1 ? 'green' : 'red') : 'gray';
+  const { lettersStatus } = useOrdle();
+  const status = lettersStatus[letter];
+  const statusColor =
+    status === 0 || status === 1 ? (status === 1 ? 'green' : 'red') : 'gray';
 
-	return (
-		<div className='Letter flex items-center justify-center gap-1 uppercase'>
-			<LetterButtonDown letter={letter} status={status} />
-			<span
-				className={`text-${statusColor}-400${status === 1 ? 'font-bold' : ''}`}
-			>
-				{letter}
-			</span>
-			<LetterButtonUp letter={letter} status={status} />
-		</div>
-	);
+  return (
+    <div className="Letter flex items-center justify-center gap-1 uppercase">
+      <LetterButtonDown letter={letter} status={status} />
+      <span
+        className={`text-${statusColor}-400${status === 1 ? ' font-bold' : ''}`}
+      >
+        {letter}
+      </span>
+      <LetterButtonUp letter={letter} status={status} />
+    </div>
+  );
 }
 
 export default Letter;
